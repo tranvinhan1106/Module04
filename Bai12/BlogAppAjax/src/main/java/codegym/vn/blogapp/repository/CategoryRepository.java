6d8d8af7466a809c974a2d65794c90186c01866b
tranvinhan1106 <117093665+tranvinhan1106@users.noreply.github.com>
2023-09-26 16:46:21 +0700

    Delete Bai12/BlogAppAjax directory

--- Bai12/BlogAppAjax/src/main/java/codegym/vn/blogapp/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package codegym.vn.blogapp.repository;
-
-import codegym.vn.blogapp.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category ,Integer> {
-}
