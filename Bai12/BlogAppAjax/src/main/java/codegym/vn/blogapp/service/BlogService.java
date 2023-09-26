4957c492350671555ea896e86dae090a841d8a67
tranvinhan1106 <117093665+tranvinhan1106@users.noreply.github.com>
2023-09-26 16:39:26 +0700

    Add files via upload

---
+++ Bai12/BlogAppAjax/src/main/java/codegym/vn/blogapp/service/BlogService.java
@@ -0,0 +1,18 @@
+package codegym.vn.blogapp.service;
+
+import codegym.vn.blogapp.entity.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import java.util.List;
+
+public interface BlogService {
+    List<Blog> findAll();
+    Blog findById(String id);
+    void create(Blog blog);
+    void delete(String id);
+    void update(Blog blog);
+    Page<Blog> findAll(Pageable pageable);
+    List<Blog> searchByName(String name);
+}
