9477eacd0ba8d59338da9404feffab372d19c447
tranvinhan1106 <117093665+tranvinhan1106@users.noreply.github.com>
2024-01-08 22:33:17 +0700

    Delete Bai10/shopping-cart directory

--- Bai10/shopping-cart/src/main/java/com/example/shoppingcart/reposiroty/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shoppingcart.reposiroty;
-
-import com.example.shoppingcart.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
